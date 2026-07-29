e98ad0f6a2ee216976fa8cc71047ce7fa5e3b264
Samruddhi <kadamsamruddhi370@gmail.com>
2026-07-29 22:38:20 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // If Vercel is building the app, use the root path. Otherwise, use the GitHub Pages path.
-  base: process.env.VERCEL ? '/' : '/Portfolio-React/', 
+  // Defaults to GitHub Pages, unless VITE_BASE_PATH is provided
+  base: process.env.VITE_BASE_PATH || '/Portfolio-React/', 
 })
